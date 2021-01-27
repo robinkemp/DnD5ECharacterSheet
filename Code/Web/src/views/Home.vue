2a7a6c505d872572730a804279f93048cf229640
Robin Kemp <robin.kemp@e-days.com>
2021-01-27 18:13:27 +0000

    Add router, witches

---
+++ Code/Web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/witches-hat.png">
+    <HelloWorld msg="Welcome witches"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWitches.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
