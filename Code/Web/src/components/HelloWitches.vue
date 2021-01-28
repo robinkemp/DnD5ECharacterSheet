b2959ccde796e924295102eedef42bfbf3678850
Raychellgram <rachelgraham838@gmail.com>
2021-01-28 19:59:42 +0000

    Rachel is amazing!

--- Code/Web/src/components/HelloWitches.vue
+++ Code/Web/src/components/HelloWitches.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Witches Hat
+          Welcome to Witches Hat (Rachel is amazing!)
         </h1>
 
         <p class="subheading font-weight-regular">
