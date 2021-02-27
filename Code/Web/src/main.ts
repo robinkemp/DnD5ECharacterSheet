e98dbaa7399405e94792e8e35b5d7968fd75a2e9
Robin Kemp <robin.kemp@e-days.com>
2021-02-27 14:23:55 +0000

    Adds Auth0 authentication

--- Code/Web/src/main.ts
+++ Code/Web/src/main.ts
@@ -3,6 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
+import { Auth0Plugin } from './auth'
+import { domain, clientId } from '../auth.config.json'
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState: any) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+})
+
 Vue.config.productionTip = false;
 
 new Vue({
